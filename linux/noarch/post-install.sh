e4bcc82b849bd268bd585a46da85f2fb97312021
mrmichalis <mihaliz@gmail.com>
2013-06-18 20:44:45 +0100

    correction mysql-connector-java.jar

--- linux/noarch/post-install.sh
+++ linux/noarch/post-install.sh
@@ -15,8 +15,9 @@ command -v java >/dev/null 2>&1 || wget http://archive.cloudera.com/cm4/redhat/6
 echo "* Downloading MySQL Connector-J ..."
 wget http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.25.tar.gz/from/http://cdn.mysql.com/ -O /root/CDH/mysql-connector-java-5.1.25.tar.gz
 tar xzvf /root/CDH/mysql-connector-java-5.1.25.tar.gz 
-[ -d /opt/cloudera/parcels/CDH/lib/hive/lib/ ] && cp /root/CDH/mysql-connector-java-5.1.25/mysql-connector-java-5.1.25-bin.jar /opt/cloudera/parcels/CDH/lib/hive/lib/
 [ -d /usr/share/java/ ] && cp /root/CDH/mysql-connector-java-5.1.25/mysql-connector-java-5.1.25-bin.jar /usr/share/java/mysql-connector-java.jar
+[ -d /opt/cloudera/parcels/CDH/lib/hive/lib/ ] && cp /root/CDH/mysql-connector-java-5.1.25/mysql-connector-java-5.1.25-bin.jar /opt/cloudera/parcels/CDH/lib/hive/lib/mysql-connector-java.jar
+[ -d /var/lib/oozie ] && ln -s /usr/share/java/mysql-connector-java.jar /var/lib/oozie/mysql-connector-java.jar
 
 echo "* ExtJS library to enable Oozie webconsole ..."
 wget http://extjs.com/deploy/ext-2.2.zip -O /root/CDH/ext-2.2.zip
