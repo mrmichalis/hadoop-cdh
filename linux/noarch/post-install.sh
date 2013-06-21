ebc9577148de5f97ac2495d4fdbf442cc6b010ce
mrmichalis <mihaliz@gmail.com>
2013-06-21 22:53:42 +0100

    replace: use curl instead of wget

--- linux/noarch/post-install.sh
+++ linux/noarch/post-install.sh
@@ -23,8 +23,7 @@ echo "* Oracle JDK 6u31 from CM..."
 command -v java >/dev/null 2>&1 || wget http://archive.cloudera.com/cm4/redhat/6/x86_64/cm/4/RPMS/x86_64/jdk-6u31-linux-amd64.rpm -O /root/CDH/jdk-6u31-linux-amd64.rpm
 
 echo "* Downloading MySQL Connector-J ..."
-wget http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.25.tar.gz/from/http://cdn.mysql.com/ -O /root/CDH/mysql-connector-java-5.1.25.tar.gz
-tar xzvf /root/CDH/mysql-connector-java-5.1.25.tar.gz 
+curl -L http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.25.tar.gz/from/http://cdn.mysql.com/ | tar xzv
 [ -d /usr/share/java/ ] && cp /root/CDH/mysql-connector-java-5.1.25/mysql-connector-java-5.1.25-bin.jar /usr/share/java/mysql-connector-java.jar
 [ -d /opt/cloudera/parcels/CDH/lib/hive/lib/ ] && cp /root/CDH/mysql-connector-java-5.1.25/mysql-connector-java-5.1.25-bin.jar /opt/cloudera/parcels/CDH/lib/hive/lib/mysql-connector-java.jar
 [ -d /var/lib/oozie ] && ln -s /usr/share/java/mysql-connector-java.jar /var/lib/oozie/mysql-connector-java.jar
