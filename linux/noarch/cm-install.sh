6c294225a40875bc42302bb98ff96ebcbd02dce4
mrmichalis <mihaliz@gmail.com>
2013-06-28 13:47:38 +0100

    change:cm-install

--- linux/noarch/cm-install.sh
+++ linux/noarch/cm-install.sh
@@ -1,11 +1,51 @@
 #!/usr/bin/env bash
+usage() {
+  echo "usage: $0 --version=4.5.1 or --bin (--embed-db)" 1>&2
+  echo "Optional: --embed-db      : install cloudera-manager-server-db" 1>&2
+  echo "Available versions: " 1>&2
+  wget -qO - http://archive.cloudera.com/cm4/redhat/6/x86_64/cm/ | awk 'BEGIN{ RS="<a *href *= *\""} NR>2 {sub(/".*/,"|");print;}' | grep "^4" | tr "/" " " | tr "\n" " "
+  echo ""
+}
 
-function install {
+function redirectHosts {
  if [ $(egrep -ic "192.168.1.245" "/etc/hosts") -eq 0 ]; then
   echo "192.168.1.245 archive.cloudera.com" >> /etc/hosts
   echo "192.168.1.245 beta.cloudera.com" >> /etc/hosts
  fi
+}
+
+function installJava {
+  command -v java >/dev/null 2>&1 || wget http://archive.cloudera.com/cm4/redhat/6/x86_64/cm/4/RPMS/x86_64/jdk-6u31-linux-amd64.rpm -O /root/CDH/jdk-6u31-linux-amd64.rpm
+  command -v java >/dev/null 2>&1 || rpm -ivh /root/CDH/jdk-6u31-linux-amd64.rpm
+}
+
+function useBinInstaller {
  ./cloudera-manager-installer.bin --i-agree-to-all-licenses --noprompt --noreadme --nooptions
+}
+
+function useRpm {
+CMVERSION=`echo $1 | sed -e 's/^[^=]*=//g'`
+rpm --import http://archive.cloudera.com/cdh4/redhat/6/x86_64/cdh/RPM-GPG-KEY-cloudera
+cat << EOF > /etc/yum.repos.d/cloudera-manager.repo
+[cloudera-manager]
+# Packages for Cloudera Manager, Version 4, on RedHat or CentOS 6 x86_64
+name=Cloudera Manager
+baseurl=http://archive.cloudera.com/cm4/redhat/6/x86_64/cm/$CMVERSION/
+gpgkey = http://archive.cloudera.com/cm4/redhat/6/x86_64/cm/RPM-GPG-KEY-cloudera
+gpgcheck = 1
+EOF
+}
+ 
+startServices() {
+ for SERVICE_NAME in cloudera-scm-server-db cloudera-scm-server cloudera-scm-agent; do
+  service $SERVICE_NAME start
+ done
+}
+ 
+stopServices() {
+ for SERVICE_NAME in cloudera-scm-server cloudera-scm-agent cloudera-scm-server-db; do
+  service $SERVICE_NAME stop
+ done
 }
 
 function managerSettings {
@@ -18,15 +58,42 @@ function managerSettings {
    service cloudera-scm-server restart
  fi
 }
+if [ $# -lt 1 ]; then
+  usage
+  exit 1
+fi
 
 set -x
-install
-
 for target in "$@"; do
   case "$target" in
-  lic)
-    (managerSettings)
+  --version*)
+    redirectHosts
+    installJava
+    stopServices
+    useRpm $target
+    yum install -y cloudera-manager-daemons cloudera-manager-server cloudera-manager-agent $SERVER_DB
+    startServices
+    shift
+    ;;
+  --embed-db)
+    stopServices
+    SERVER_DB=${SERVER_DB:-cloudera-manager-server-db}
+    yum install -y cloudera-manager-daemons cloudera-manager-server cloudera-manager-agent $SERVER_DB
+    startServices
+    shift
+    ;;
+  --lic)
+    managerSettings
+    shift
     ;;  
+  --bin)
+    redirectHosts
+    useBinInstaller
+    shift
+    ;;  
+  *)
+    usage
+    exit 1    
   esac
 done
 
