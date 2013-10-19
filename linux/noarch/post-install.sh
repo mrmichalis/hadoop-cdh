8ec9424976aeb2318f7ef11c446f212f79e87fdc
mrmichalis <mihaliz@gmail.com>
2013-10-19 21:30:03 +0100

    changed hosts

--- linux/noarch/post-install.sh
+++ linux/noarch/post-install.sh
@@ -29,8 +29,8 @@ if grep -q -i "Red Hat" /etc/redhat-release; then
   sed -i 's/1/0/g' /etc/yum/pluginconf.d/product-id.conf 
   sed -i 's/1/0/g' /etc/yum/pluginconf.d/subscription-manager.conf
 fi
-echo "192.168.1.245 archive.cloudera.com" >> /etc/hosts
-echo "192.168.1.245 beta.cloudera.com" >> /etc/hosts
+echo "192.168.88.250 archive.cloudera.com" >> /etc/hosts
+echo "192.168.88.250 beta.cloudera.com" >> /etc/hosts
 mkdir -p /root/CDH
 #end -init
 
