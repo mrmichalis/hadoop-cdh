fdf530e13eebc97fcdb2a33c3896fb21e0dd8afc
mrmichalis <mihaliz@gmail.com>
2013-11-06 18:41:25 +0000

    added java 7

--- linux/noarch/orajava-install.sh
+++ linux/noarch/orajava-install.sh
@@ -2,6 +2,7 @@
 echo "* Downloading and Installing Oracle Java SDK 6u45 from Oracle ..."
 #32-bit http://download.oracle.com/otn/java/jdk/6u45-b06/jdk-6u45-linux-i586-rpm.bin
 #64-bit http://download.oracle.com/otn-pub/java/jdk/6u45-b06/jdk-6u45-linux-x64-rpm.bin
+#Java 7 64-bit http://download.oracle.com/otn-pub/java/jdk/7u45-b18/jdk-7u45-linux-x64.rpm
 JAVA_URL="http://download.oracle.com/otn-pub/java/jdk/6u45-b06/jdk-6u45-linux-x64-rpm.bin"
 JAVA_FILENAME=$(echo $JAVA_URL | cut -d/ -f8)
 wget --no-check-certificate --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com" $JAVA_URL -O /root/CDH/$JAVA_FILENAME
