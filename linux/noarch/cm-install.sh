f33af5129e70441e580c9296720bfae1c83719e0
mrmichalis <mihaliz@gmail.com>
2013-12-12 21:17:32 +0000

    jdk

--- linux/noarch/cm-install.sh
+++ linux/noarch/cm-install.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 START_SCM_AGENT=''
 SERVER_DB=''
+JDK_VER=''
 
 usage() {
   VERTMP=$(wget -qO - http://archive.cloudera.com/cm4/redhat/6/x86_64/cm/ | awk 'BEGIN{ RS="<a *href *= *\""} NR>2 {sub(/".*/,"|");print;}' | grep "^4" | tr "/" " " | tr "\n" " ")
-  echo "usage: $0 --bin or --version=4.6.0 [--embed-db OR --mysql-db]" 1>&2
+  echo "usage: $0 --bin or --version=4.6.0 [--embed-db OR --mysql-db] --jdk=[6 or 7]" 1>&2
   echo "Options" 1>&2
   echo "  --bin              :   Use latest installer" 1>&2
   echo "  --version=[4.2.1]  :   Install/Upgrade version" 1>&2
@@ -14,6 +15,8 @@ usage() {
   echo "  --embed-db         :   Install/Upgrade cloudera-manager-server-db" 1>&2
   echo "  --mysql-db         :   Prepare MySQL Database" 1>&2
   echo " "
+  echo "JDK (default JDK6)" 1>&2
+  echo "  --jdk=[6 or 7]     :   Install with JDK 6 or JDK 7" 1>&2
 }
 
 function promptyn () {
@@ -27,28 +30,37 @@ function promptyn () {
   done
 }
 
-function redirectHosts {
- if [ $(egrep -ic "192.168.88.250" "/etc/hosts") -eq 0 ]; then
-  echo "192.168.88.250 archive.cloudera.com" >> /etc/hosts
-  echo "192.168.88.250 beta.cloudera.com" >> /etc/hosts
- fi
-}
-
 function installJava {
-  command -v java >/dev/null 2>&1 || wget http://archive.cloudera.com/cm4/redhat/6/x86_64/cm/4/RPMS/x86_64/jdk-6u31-linux-amd64.rpm -O /root/CDH/jdk-6u31-linux-amd64.rpm
-  command -v java >/dev/null 2>&1 || rpm -ivh /root/CDH/jdk-6u31-linux-amd64.rpm
+  JDK_VER=`echo $1 | sed -e 's/^[^=]*=//g'`
+  if [ $JDK_VER -ne "7" ]; then 
+    echo "* Oracle JDK 6u31 from CM..."
+    command -v java >/dev/null 2>&1 || wget http://archive.cloudera.com/cm4/redhat/6/x86_64/cm/4/RPMS/x86_64/jdk-6u31-linux-amd64.rpm -O /root/CDH/jdk-6u31-linux-amd64.rpm
+    command -v java >/dev/null 2>&1 || rpm -ivh /root/CDH/jdk-6u31-linux-amd64.rpm
+    
+    echo "* Downloading Java Cryptography Extension (JCE) ..."
+    wget --no-check-certificate --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com" http://download.oracle.com/otn-pub/java/jce_policy/6/jce_policy-6.zip -O /root/CDH/jce_policy-6.zip
+    [[ -d "/usr/java/default/jre/lib/security/" ]] && unzip -oj /root/CDH/jce_policy-6.zip -d /usr/java/default/jre/lib/security/   
+  else
+    echo "* Oracle JDK 7u25 from CM..."
+    command -v java >/dev/null 2>&1 || wget http://archive.cloudera.com/cm5/redhat/6/x86_64/cm/5/RPMS/x86_64/oracle-j2sdk1.7-1.7.0+update25-1.x86_64.rpm -O /root/CDH/oracle-j2sdk1.7-1.7.0+update25-1.x86_64.rpm 
+    command -v java >/dev/null 2>&1 || rpm -ivh /root/CDH/oracle-j2sdk1.7-1.7.0+update25-1.x86_64.rpm
+    
+    echo "* Downloading Java Cryptography Extension (JCE 7) ..."
+    wget --no-check-certificate --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com" http://download.oracle.com/otn-pub/java/jce/7/UnlimitedJCEPolicyJDK7.zip -O /root/CDH/UnlimitedJCEPolicyJDK7.zip
+    [[ -d "/usr/java/default/jre/lib/security/" ]] && unzip -oj /root/CDH/UnlimitedJCEPolicyJDK7.zip -d /usr/java/default/jre/lib/security/
+  fi
 }
 
 function useBinInstaller {
-./cloudera-manager-installer.bin --i-agree-to-all-licenses --noprompt --noreadme --nooptions
-#./cloudera-manager-installer.bin --use_embedded_db=0 --db_pw=cloudera_scm --no-prompt --i-agree-to-all-licenses --noreadme
+  ./cloudera-manager-installer.bin --i-agree-to-all-licenses --noprompt --noreadme --nooptions
+  #./cloudera-manager-installer.bin --use_embedded_db=0 --db_pw=cloudera_scm --no-prompt --i-agree-to-all-licenses --noreadme
 }
 
 function useRpm {
-yum clean all
-CMVERSION=`echo $1 | sed -e 's/^[^=]*=//g'`
-rpm --import http://archive.cloudera.com/cdh4/redhat/6/x86_64/cdh/RPM-GPG-KEY-cloudera
-cat << EOF > /etc/yum.repos.d/cloudera-manager.repo
+  yum clean all
+  CMVERSION=`echo $1 | sed -e 's/^[^=]*=//g'`
+  rpm --import http://archive.cloudera.com/cdh4/redhat/6/x86_64/cdh/RPM-GPG-KEY-cloudera
+  cat << EOF > /etc/yum.repos.d/cloudera-manager.repo
 [cloudera-manager]
 # Packages for Cloudera Manager, Version 4, on RedHat or CentOS 6 x86_64
 name=Cloudera Manager
@@ -89,8 +101,6 @@ set -x
 for target in "$@"; do
   case "$target" in
   --version*)
-    redirectHosts
-    installJava    
     useRpm $target
     yum install -y cloudera-manager-daemons cloudera-manager-server cloudera-manager-agent
     # if [ -z $START_SCM_AGENT ] && promptyn "Do you wish to start cloudera-scm-agent? [y/n]"; then 
@@ -117,10 +127,13 @@ for target in "$@"; do
     shift
     ;;  
   --bin)
-    redirectHosts
     useBinInstaller
     shift
     ;;  
+  --jdk*)
+    installJava $target
+    shift
+    ;;
   *)
     usage
     exit 1    
