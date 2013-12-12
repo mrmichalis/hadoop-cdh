c6fbd50d0d7c26dfc885d75967cf171a963aba5b
mrmichalis <mihaliz@gmail.com>
2013-12-12 11:24:27 +0000

    fix

--- linux/noarch/mvn-ant-install.sh
+++ linux/noarch/mvn-ant-install.sh
@@ -5,12 +5,12 @@
 #tar xzvf apache-maven-3.1.1-bin.tar.gz -C /usr/local/ && ln -s /usr/local/apache-maven-3.1.1 /usr/local/maven
 
 echo "Install Maven 2.2.1"
-curl -L http://apache.mirrors.timporter.net/maven/maven-2/2.2.1/binaries/apache-maven-2.2.1-bin.tar.gz -o apache-maven-2.2.1-bin.tar.gz 
+curl -L http://www.mirrorservice.org/sites/ftp.apache.org/maven/maven-2/2.2.1/binaries/apache-maven-2.2.1-bin.tar.gz -o apache-maven-2.2.1-bin.tar.gz
 tar xzvf apache-maven-2.2.1-bin.tar.gz -C /usr/local/ && ln -s /usr/local/apache-maven-2.2.1/ /usr/local/maven
 
 echo "Install Ant 1.9.2"
-curl -L http://mirror.catn.com/pub/apache//ant/binaries/apache-ant-1.9.2-bin.tar.gz -o apache-ant-1.9.2-bin.tar.gz
-tar xzvf apache-ant-1.9.2-bin.gz -C /usr/local/ && ln -s /usr/local/apache-ant-1.9.2/ /usr/local/ant
+curl -L http://apache.mirror.anlx.net//ant/binaries/apache-ant-1.9.2-bin.tar.gz -o apache-ant-1.9.2-bin.tar.gz
+tar xzvf apache-ant-1.9.2-bin.tar.gz -C /usr/local/ && ln -s /usr/local/apache-ant-1.9.2/ /usr/local/ant
 
 echo "Modify .bash_profile"
 sed -i "s/PATH=/#PATH=/g" ~/.bash_profile
@@ -18,8 +18,8 @@ sed -i "s/export PATH/#export PATH/g" ~/.bash_profile
 
 cat << EOF >> ~/.bash_profile
 JAVA_HOME=/usr/java/default
-ANT_HOME=/usr/local/ant/lib
+ANT_HOME=/usr/local/ant
 M2_HOME=/usr/local/maven
-PATH=${JAVA_HOME}/bin:${M2_HOME}/bin:${ANT_HOME}/bin:${PATH}:${HOME}/bin
+PATH=\${JAVA_HOME}/bin:\${M2_HOME}/bin:\${ANT_HOME}/bin:\${PATH}:\${HOME}/bin
 export PATH M2_HOME ANT_HOME JAVA_HOME
 EOF
