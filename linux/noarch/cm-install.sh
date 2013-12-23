080b0fda7cba9851042a23f7e965eb026fd0cc28
mrmichalis <mihaliz@gmail.com>
2013-12-23 18:07:13 +0000

    bump: mysql connector 5.1.28

--- linux/noarch/cm-install.sh
+++ linux/noarch/cm-install.sh
@@ -55,9 +55,9 @@ function installJava {
     fi
   fi
   echo "* Install MySQL Connector ..."
-  curl -L http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.26.tar.gz/from/http://cdn.mysql.com/ | tar xzv
-  [[ -d "/usr/share/java/" && ! -e "/usr/share/java/mysql-connector-java.jar" ]] && cp /root/CDH/mysql-connector-java-5.1.26/mysql-connector-java-5.1.26-bin.jar /usr/share/java/mysql-connector-java.jar
-  [[ -d "/opt/cloudera/parcels/CDH/lib/hive/lib/" && ! -e "/opt/cloudera/parcels/CDH/lib/hive/lib/mysql-connector-java.jar" ]] && ln -s /root/CDH/mysql-connector-java-5.1.26/mysql-connector-java-5.1.26-bin.jar /opt/cloudera/parcels/CDH/lib/hive/lib/mysql-connector-java.jar      
+  curl -L http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.28.tar.gz | tar xzv
+  [[ -d "/usr/share/java/" && ! -e "/usr/share/java/mysql-connector-java.jar" ]] && cp /root/CDH/mysql-connector-java-5.1.28/mysql-connector-java-5.1.28-bin.jar /usr/share/java/mysql-connector-java.jar
+  [[ -d "/opt/cloudera/parcels/CDH/lib/hive/lib/" && ! -e "/opt/cloudera/parcels/CDH/lib/hive/lib/mysql-connector-java.jar" ]] && ln -s /root/CDH/mysql-connector-java-5.1.28/mysql-connector-java-5.1.28-bin.jar /opt/cloudera/parcels/CDH/lib/hive/lib/mysql-connector-java.jar      
   
 }
 
