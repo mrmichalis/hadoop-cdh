8e7e4c9a9cc4c6f42d01d8e231ac450eccc4e7dd
mrmichalis <mihaliz@gmail.com>
2013-12-30 14:02:21 +0000

    screen

--- linux/noarch/post-install.sh
+++ linux/noarch/post-install.sh
@@ -16,7 +16,7 @@ if [ "\$PS1" != "" -a "\${STARTED_SCREEN:-x}" = x -a "\${SSH_TTY:-x}" != x ]
 then
   STARTED_SCREEN=1 ; export STARTED_SCREEN
   [ -d \$HOME/lib/screen-logs ] || mkdir -p \$HOME/lib/screen-logs
-  sleep `
+  sleep 1
   screen -RR && exit 0
   # normally, execution of this rc script ends here...
   echo "Screen failed! continuing with normal bash startup"
