d98dcebca36d80210ed9dd3307991b3bab5b7c98
mrmichalis <mihaliz@gmail.com>
2014-01-17 20:09:22 +0000

    API

--- linux/noarch/cm-api-default.sh
+++ linux/noarch/cm-api-default.sh
@@ -1,4 +1,4 @@
-curl -X PUT -H 'Content-Type:application/json' -u admin:admin -d '{ 
+curl -X PUT -H 'Content-Type:application/json' -u admin:admin -d '{
 "items" : [
   {
     "name" : "SESSION_TIMEOUT",
@@ -7,34 +7,41 @@ curl -X PUT -H 'Content-Type:application/json' -u admin:admin -d '{
 ]
 }' http://$(hostname -f):7180/api/v4/cm/config
 
-curl -X PUT -H 'Content-Type:application/json' -u admin:admin -d '{ 
-"items" : [  
+curl -X PUT -H 'Content-Type:application/json' -u admin:admin -d '{
+"items" : [
+  {
+    "name" : "firehose_activity_purge_duration_hours",
+    "value" : "24"
+  },
   {
     "name" : "firehose_attempt_purge_duration_hours",
     "value" : "24"
-  },  
+  },
   {
     "name" : "timeseries_expiration_hours",
     "value" : "24"
   }
-] 
+]
 }' http://$(hostname -f):7180/api/v4/cm/service/roleConfigGroups/mgmt1-ACTIVITYMONITOR-BASE/config
 
-curl -X PUT -H 'Content-Type:application/json' -u admin:admin -d '{ 
-"items" : [  
+curl -X PUT -H 'Content-Type:application/json' -u admin:admin -d '{
+"items" : [
   {
     "name" : "timeseries_expiration_hours",
     "value" : "24"
   }
-] 
-}' http://$(hostname -f):7180/api/v4/cm/service/roleConfigGroups/mgmt1-SERVICEMONITOR-BASE/config 
+]
+}' http://$(hostname -f):7180/api/v4/cm/service/roleConfigGroups/mgmt1-SERVICEMONITOR-BASE/config
 
-  
-curl -X PUT -H 'Content-Type:application/json' -u admin:admin -d '{ 
-"items" : [  
+
+curl -X PUT -H 'Content-Type:application/json' -u admin:admin -d '{
+"items" : [
   {
     "name" : "timeseries_expiration_hours",
     "value" : "24"
   }
-] 
+]
 }' http://$(hostname -f):7180/api/v4/cm/service/roleConfigGroups/mgmt1-HOSTMONITOR-BASE/config
+
+
+curl -X POST -u admin:admin http://$(hostname -f):7180/api/v4/cm/service/commands/restart
