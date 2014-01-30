2e57c952676a81f5fac4119bdab872888a9a1991
mrmichalis <mihaliz@gmail.com>
2014-01-30 21:41:19 +0000

    JAVA_HOME

--- linux/noarch/orajava-install.sh
+++ linux/noarch/orajava-install.sh
@@ -8,3 +8,5 @@ JAVA_FILENAME=$(echo $JAVA_URL | cut -d/ -f8)
 wget --no-check-certificate --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com" $JAVA_URL -O /root/CDH/$JAVA_FILENAME
 chmod +x /root/CDH/$JAVA_FILENAME
 touch answers && sh $JAVA_FILENAME < answers && /bin/rm answers
+echo "* Set JAVA_HOME in /etc/profile.d/java.sh ..."
+echo 'export JAVA_HOME=/usr/java/default' > /etc/profile.d/java.sh
