c5c29059bbc36cdd1e4ef5c87019b836462d8e10
mrmichalis <mihaliz@gmail.com>
2014-05-24 01:24:46 +0100

    minor

--- linux/noarch/cm-api-default.sh
+++ linux/noarch/cm-api-default.sh
@@ -57,4 +57,6 @@ curl -X PUT -H 'Content-Type:application/json' -u admin:admin -d '{
 
 curl -X POST -u admin:admin http://$(hostname -f):7180/api/v4/cm/service/commands/restart
 
-curl -u admin:admin -X POST http://$(hostname -f):7180/api/v5/clusters/Cluster%201%20-%20CDH4/services/mapreduce1/roleCommands/refresh -H "Content-Type:application/json" -d '{"items":["mapreduce1-JOBTRACKER-6f46bf2f3bd625ffe9b041c0f725fbf4"]}'
+# curl -u admin:admin -X POST http://$(hostname -f):7180/api/v5/clusters/Cluster%201%20-%20CDH4/services/mapreduce1/roleCommands/refresh -H "Content-Type:application/json" -d '{"items":["mapreduce1-JOBTRACKER-6f46bf2f3bd625ffe9b041c0f725fbf4"]}'
+# curl -u admin:admin -X POST http://$(hostname -f):7180/api/v5/clusters/Cluster%201%20-%20CDH4/services/hdfs1/config -H "Content-Type:application/json" -d '{ "items" : [ {"name" : "dfs_replication","value" : "1"} ]}'
+
