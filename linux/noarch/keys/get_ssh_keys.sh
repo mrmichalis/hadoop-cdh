4e85e40ae16c2460a5248795a3cf9a0953fc5f3b
mrmichalis <mihaliz@gmail.com>
2014-10-19 14:14:56 +0100

    authorized keys

--- linux/noarch/keys/get_ssh_keys.sh
+++ linux/noarch/keys/get_ssh_keys.sh
@@ -2,8 +2,8 @@
 echo "* Installing SSH keys..."
 mkdir -p /root/.ssh
 chmod 700 /root/.ssh
-wget --no-check-certificate -nv 'https://raw.githubusercontent.com/gdgt/cmapi/master/keys/authorized_keys' -O /root/.ssh/authorized_keys
-wget --no-check-certificate -nv 'https://raw.githubusercontent.com/gdgt/cmapi/master/keys/id_rsa' -O /root/.ssh/id_rsa
-wget --no-check-certificate -nv 'https://raw.githubusercontent.com/gdgt/cmapi/master/keys/id_rsa.pub' -O /root/.ssh/id_rsa.pub
+wget --no-check-certificate -nv 'https://raw.githubusercontent.com/mrmichalis/hadoop-cdh/master/linux/noarch/keys/authorized_keys' -O /root/.ssh/authorized_keys
+wget --no-check-certificate -nv 'https://raw.githubusercontent.com/mrmichalis/hadoop-cdh/master/linux/noarch/keys/id_rsa' -O /root/.ssh/id_rsa
+wget --no-check-certificate -nv 'https://raw.githubusercontent.com/mrmichalis/hadoop-cdh/master/linux/noarch/keys/id_rsa.pub' -O /root/.ssh/id_rsa.pub
 chmod 600 /root/.ssh/authorized_keys /root/.ssh/id_rsa /root/.ssh/id_rsa.pub
 chown -R root /root/.ssh
